37a9c5eeef861be332028846d85f65549d2c6fc2
Greg <gmengler@gmail.com>
2017-05-03 13:43:03 -0600

    category entity complete

--- CollectorsREST/src/controllers/CategoryController.java
+++ CollectorsREST/src/controllers/CategoryController.java
@@ -1,10 +1,53 @@
 package controllers;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
+import entities.Item;
 
+@Entity
 public class CategoryController {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 	
-	/// stuff
-//	greg comment
+	private String name;
+	
+	@OneToOne(mappedBy="category")
+    private Item item;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	@Override
+	public String toString() {
+		return "CategoryController [id=" + id + ", name=" + name + ", item=" + item + "]";
+	}
+	
+	
 }
