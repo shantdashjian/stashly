af3b03aa92c2a143ed6fc699ef55d1b90529aff5
EhrenWilliamson <ehren.williamson2@gmail.com>
2017-05-03 15:40:40 -0500

    add itemlist and other ngRoutes

---
+++ CollectorsREST/WebContent/app/item/itemList/itemList.component.js
@@ -0,0 +1,12 @@
+angular.module('item')
+.component('itemList', {
+	templateUrl: 'app/item/itemList/itemList.component.html',
+	controller: function(){
+		var vm = this;
+		
+		
+	},
+	
+	controllerAs: 'vm'
+	
+})
