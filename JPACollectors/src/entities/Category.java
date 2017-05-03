3204de72fc59372341fa888a8d01de35b19dd942
Greg <gmengler@gmail.com>
2017-05-03 13:52:42 -0600

    fixed category entity

--- JPACollectors/src/entities/Category.java
+++ JPACollectors/src/entities/Category.java
@@ -1,10 +1,14 @@
 package entities;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="category")
 public class Category {
 	
 	@Id
