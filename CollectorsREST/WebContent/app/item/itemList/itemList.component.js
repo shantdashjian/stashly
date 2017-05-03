5ce399c324ca4e4b4a724e0c07a3bec8748bb4a9
EhrenWilliamson <ehren.williamson2@gmail.com>
2017-05-03 16:26:09 -0500

    data changes after test trials

--- CollectorsREST/WebContent/app/item/itemList/itemList.component.js
+++ CollectorsREST/WebContent/app/item/itemList/itemList.component.js
@@ -5,10 +5,17 @@ angular.module('item')
 		var vm = this;
 		vm.items = []
 		vm.reload = function(){
-			itemService.index().then(function(response){
-				vm.items = response.data
+//			itemService.index().then(function(response){
+//				vm.items = response.data
+				vm.items = [{
+						imageUrl: 'http://i.ebayimg.com/images/g/XbEAAOSww9xZA~V-/s-l1600.jpg',
+						name: "stuff",
+						currentValue: 220,
+						purchasePrice : 10,
+						purchaseDate: '1989-07-07'
+				}]
 				
-			})
+//			})
 		}
 		vm.reload();
 	},
