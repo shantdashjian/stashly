1db5e8cf9514415cf0617f0debd45a7fb657e313
Dburn22 <david.burnett2@yahoo.com>
2017-05-04 14:30:41 -0600

    logout working

--- CollectorsREST/WebContent/app/authModule/logout/logout.component.js
+++ CollectorsREST/WebContent/app/authModule/logout/logout.component.js
@@ -3,23 +3,7 @@ angular.module("authModule").component("logout", {
 	controller : function(authService, $location, $cookies) {
 		var vm = this;
 		
-		vm.hideNavbar = function(){
-			if(authService.getToken().id){
-				return false;
-			}
-			else{
-				return false;
-			}
-		}
 		
-		vm.logout = function() {
-			authService.logout().then(function(res) {
-				vm.hideNavbar();
-				$location.path('/')
-
-			})
-
-		}
 	},
 	controllerAs : 'vm'
 });
