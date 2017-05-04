5fdb289522f68a74981bdf232f3d3dbba0bd5e05
Shaun Dashjian <shaundashjian@yahoo.com>
2017-05-04 09:41:02 -0600

    Pushing working login

---
+++ CollectorsREST/WebContent/app/item/itemShow/itemShow.component.js
@@ -0,0 +1,9 @@
+angular.module('item')
+.component('itemShow', {
+	templateUrl : 'app/item/itemShow/itemShow.component.html',
+	controller: function(){
+		var vm = this;
+	},
+	controllerAs: 'vm'
+})
+	
