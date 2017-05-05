44edc286361c0cf2bb38a861aa8ea4a2b0f82f98
Dburn22 <david.burnett2@yahoo.com>
2017-05-05 13:09:21 -0600

    filter

---
+++ CollectorsREST/WebContent/app/buttons/button.component.js
@@ -0,0 +1,19 @@
+//'use strict';
+//var controllerId = 'qtestController';
+//
+//angular.module('appModule').controller(controllerId,
+//		[ '$scope', 'qtestDataService', qtestController ]), 
+//	
+//	
+//
+//	function qtestController($scope, qtestDataService) {
+//		var vm = this;
+//		vm.biddingHistory = [];
+//		vm.getBids = getBids;
+//
+//		function getbids() {
+//			vm.biddingHistory = qtestDataService.getDataQ();
+//			return vm.biddingHistory;
+//		}
+//	
+//}
