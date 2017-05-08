c63fc391d0a7f6614373857f624e494446a22ddc
Greg <gmengler@gmail.com>
2017-05-08 15:11:33 -0600

    various styling

--- CollectorsREST/WebContent/app/authModule/login/login.component.js
+++ CollectorsREST/WebContent/app/authModule/login/login.component.js
@@ -1,7 +1,10 @@
 angular.module("authModule").component("login", {
 	templateUrl : 'app/authModule/login/login.component.html',
-	controller : function(authService, $location) {
+	controller : function(authService, $location, $document) {
 		var vm = this;
+		
+		var body = $document.find('body').eq(0);
+
 
 		vm.login = function(user) {
 			authService.login(user).then(function(res) {
@@ -13,6 +16,10 @@ angular.module("authModule").component("login", {
 		vm.signup = function(){
 			$location.path('/signup')
 		}
+		
+		
+		body.css("background-image", "url('" + 'images/captainamerica.jpg' + "')");
+		body.css("background-size", "cover");
 	},
 	controllerAs : 'vm'
 });
