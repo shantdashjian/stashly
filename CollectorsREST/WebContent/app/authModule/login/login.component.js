5d079d0a4942cf981f9b452c51d33886ffe3eaf7
Shaun Dashjian <shaundashjian@yahoo.com>
2017-05-08 14:26:55 -0600

    Added login form validation

--- CollectorsREST/WebContent/app/authModule/login/login.component.js
+++ CollectorsREST/WebContent/app/authModule/login/login.component.js
@@ -2,11 +2,14 @@ angular.module("authModule").component("login", {
 	templateUrl : 'app/authModule/login/login.component.html',
 	controller : function(authService, $location) {
 		var vm = this;
-
+		vm.error = false;
 		vm.login = function(user) {
-			authService.login(user).then(function(res) {
-
+			authService.login(user)
+			.then(function(res) {
 				$location.path('/itemList')
+			})
+			.catch(function(error){
+				vm.error = true;
 			})
 		}
 		
