f049fdfdffe0e006bb5a3de2442536818b1274c0
Greg <gmengler@gmail.com>
2017-05-09 08:38:56 -0600

    sign up stylized

--- CollectorsREST/WebContent/app/authModule/signup/signup.component.js
+++ CollectorsREST/WebContent/app/authModule/signup/signup.component.js
@@ -1,12 +1,18 @@
 angular.module("authModule").component("signup",{
 	templateUrl : 'app/authModule/signup	/signup.component.html',
-	controller : function(authService, $location) {
+	controller : function(authService, $location, $document) {
 		var vm = this;
+		
+		var body = $document.find('body').eq(0);
+		
+		body.css("background-image", "url('" + 'images/captainamerica.jpg' + "')");
+		body.css("background-size", "cover");
+		
 		vm.signup = function(user) {
 			console.log(user)
 			authService.signup(user).then(function(res) {
+				body.css("background-image", "url('')");
 				$location.path('/itemList')
-
 			})
 		}
 	},
